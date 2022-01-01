0200fbdc48f7ee0166a0ce306a861c36fdbc11d2
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-01-01 15:05:43 +0800

    Change Api Sports

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -3,7 +3,7 @@ import clubs from './clubs.js';
 export default class DataSource {
     static searchClub(keyword) {
         return fetch(
-            `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`
+            `https://sports-api.dicoding.dev/teams/search?t=${keyword}`
         )
             .then(response => response.json())
             .then(responseJson => {
